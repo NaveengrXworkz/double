9dc7188f3d84e959118ff212d8e46b275576e557
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-20 18:17:55 +0530

    double programme

---
+++ Double.java
@@ -0,0 +1,15 @@
+class Double{
+     static double []cgpa = {5.7231,7.544341,6.1172836,5.11236187,6.225617,5.4237187,5.92671,4.11451167,5.126461,5.6271382};
+     public static void main(String[] args){
+     System.out.println(cgpa[0]);
+     System.out.println(cgpa[1]);
+     System.out.println(cgpa[2]);
+     System.out.println(cgpa[3]);
+     System.out.println(cgpa[4]);
+     System.out.println(cgpa[5]);
+     System.out.println(cgpa[6]);
+     System.out.println(cgpa[7]);
+     System.out.println(cgpa[8]);
+     System.out.println(cgpa[9]);
+     }
+}
